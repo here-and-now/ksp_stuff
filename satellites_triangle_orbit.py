f29ec0e8a1b8e6121efa081080d3fd3df979190d
os <os@noreply.invalid>
2022-07-29 00:33:43 +0200

    sas and sas control

--- satellites_triangle_orbit.py
+++ satellites_triangle_orbit.py
@@ -1,4 +1,8 @@
 import krpc
+import tabulate
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 conn = krpc.connect(name="Launch into orbit")
 print("Connected to kRPC")
@@ -16,7 +20,7 @@ def select_vessel_and_duplicates_by_name(vessel_name):
     if len(vessel_list) == 0:
         print("No vessel found with name: " + vessel_name)
         return False
-    if len(vessel_list) == 1:
+    elif len(vessel_list) == 1:
         print("Vessel found: " + vessel_list[0].name)
         return vessel_list[0]
     else:
@@ -25,31 +29,14 @@ def select_vessel_and_duplicates_by_name(vessel_name):
 
         #ask for input which vessel to use, default 0
         print("Multiple vessels found:")
-        import tabulate
         #print vessel list names and met in tabulated format with index
         print(tabulate.tabulate([[i, v.name, v.met] for i, v in enumerate(vessel_list)], headers=['Index', 'Name', 'MET']))
-
-        vessel_index = int(input("Select vessel by index: "))
+        # vessel_index = int(input("Select vessel by index: "))
+        vessel_index = 0
         print("Vessel selected: " + vessel_list[vessel_index].name)
+
+
         return vessel_list[vessel_index]
-
-
-        
-        
-        for i in range(len(vessel_list)):
-            print(str(i) + ": " + vessel_list[i].name + " (" + str(vessel_list[i].met) + ")")
-        vessel_index = input("Select vessel by index (default 0): ")
-        if vessel_index == "":
-            vessel_index = 0
-        else:
-            vessel_index = int(vessel_index)
-        if vessel_index < 0 or vessel_index >= len(vessel_list):
-            print("Invalid index: " + str(vessel_index))
-            return False
-        print("Vessel found: " + vessel_list[vessel_index].name)
-        return vessel_list[vessel_index]
-
-        return vessel_list
 
     return None
 
@@ -59,27 +46,43 @@ def select_vessel_by_id(vessel_id):
             return v
     return None
 
-def switch_vessel_if_needed(vessel):
+def switch_vessel(vessel):
     if vessel != sc.active_vessel:
         backup_vessel = sc.active_vessel
         sc.active_vessel = vessel
-        print("Switched to vessel: " + vessel.name + "from " + backup_vessel.name)
-        return True
+        print("Switched to vessel: " + vessel.name + " from " + backup_vessel.name)
+        return sc.active_vessel
     else:
         print("Vessel is already active: " + vessel.name)
-    return False
+        return vessel
 
 # vessel_name = 'Kerbin_Goresat/VS-3/Magneto/MSIP'
 vessel_name = 'OsNet_1.0_Ring_1'
 
 vessel = select_vessel_and_duplicates_by_name(vessel_name)
-# switch_vessel_if_needed(vessel)
+vessel = switch_vessel(vessel)
+
+control = vessel.control
+control.sas = True
+control.sas_mode = sc.SASMode.prograde
+print(vessel.control.sas_mode)
+#activate next stage returns list of vesse
+# vessels = control.activate_next_stage()
+
+print(vessels)
 
 
 # Set up streams for telemetry
-# ut = conn.add_stream(getattr, conn.space_center, 'ut')
-# altitude = conn.add_stream(getattr, vessel.flight(), 'mean_altitude')
-# apoapsis = conn.add_stream(getattr, vessel.orbit, 'apoapsis_altitude')
+ut = conn.add_stream(getattr, conn.space_center, 'ut')
+altitude = conn.add_stream(getattr, vessel.flight(), 'mean_altitude')
+apoapsis = conn.add_stream(getattr, vessel.orbit, 'apoapsis_altitude')
+periapsis = conn.add_stream(getattr, vessel.orbit, 'periapsis_altitude')
+orbit_period = conn.add_stream(getattr, vessel.orbit, 'period')
+orbit_speed = conn.add_stream(getattr, vessel.orbit, 'speed')
+orbit_inclination = conn.add_stream(getattr, vessel.orbit, 'inclination')
+orbit_eccentricity = conn.add_stream(getattr, vessel.orbit, 'eccentricity')
+
+conn.close()
 
 # control stuff
 # control = vessel.control
@@ -96,4 +99,3 @@ vessel = select_vessel_and_duplicates_by_name(vessel_name)
 # apo = planner.operation_apoapsis
 # apo.new_apoapsis = 1000000000
 # apo.make_nodes()
-# conn.close()

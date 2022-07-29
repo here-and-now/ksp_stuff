5b2721f8982bb933f0a6bbee73ccd1f16228a817
os <os@noreply.invalid>
2022-07-29 19:43:35 +0200

    maneuver planning and node execution

--- satellites_triangle_orbit.py
+++ satellites_triangle_orbit.py
@@ -7,6 +7,7 @@ import time
 from utils.handle_vessels import switch_vessel, select_vessel_and_duplicates_by_name, activate_engines_by_name, decouple_by_name
 from utils.handle_orientation import orientate_vessel
 
+
 conn = krpc.connect(name="Launch into orbit")
 print("Connected to kRPC")
 
@@ -29,6 +30,8 @@ vessel = orientate_vessel(conn, vessel, orientation)
 activated_engines = activate_engines_by_name(vessel, 'orbital-engine-0625')
 # decouple_by_name(vessel, 'proceduralStackDecoupler')
 
+
+
 constellation_name = 'TripleOs'
 constellation_list = control.activate_next_stage()
 constellation_list.append(vessel)
@@ -38,44 +41,68 @@ for i, vessel in enumerate(constellation_list):
     # sc.active_vessel = switch_vessel(sc.active_vessel, vessel)
 
 # wait for satellites to spread apart
-time.sleep(30)
+time.sleep(10)
 
 # satellite operations
-result = [orientate_vessel(conn, vessel, 'retrograde', block=False) for vessel in constellation_list]
+# result = [orientate_vessel(conn, vessel, 'retrograde', block=False) for vessel in constellation_list]
 
-#set deploy true for every solar panel in every vessel in constellation_list
+# solar = [v.parts.solar_panels for v in constellation_list]
+# solar = [item for sublist in solar for item in sublist]
+# for panel in solar:
+    # panel.deployed = True
 
-solar = [v.parts.solar_panels for v in constellation_list]
-solar = [item for sublist in solar for item in sublist]
-for panel in solar:
-    panel.deployed = True
-print(solar)
-# for sol in solar:
-    # for p in sol:
-        # p.deployed = True
-# # # solar = [s.deploy=True for s in v.parts.solar_panels for v in constellation_list]
-# solar = [True for p in sol for sol in solar for v.parts.solar_panels for v in constellation_list]
 # throttle = [0.5 for vessel in constellation_list]
 
 # mechanical jebediah time <3
-planner = mj.maneuver_planner
-aps = planner.operation_apoapsis
-aps.new_apoapsis = 5000000
-aps.make_nodes()
+
+def execute_nodes():
+    print("Executing nodes")
+    executor = mj.node_executor
+    # executor.execute_all_nodes()
+    executor.execute_one_node()
 
 
+    with conn.stream(getattr, executor, 'enabled') as enabled:
+        enabled.rate = 1
+        with enabled.condition:
+            while enabled():
+                enabled.wait()
+                # print("Enabled: {}".format(enabled()))
+
+node_list = []
+for vessel in constellation_list:
+    sc.active_vessel = switch_vessel(sc.active_vessel, vessel)
+
+    planner = mj.maneuver_planner
+    aps = planner.operation_circularize
+    # aps.new_apoapsis = 200000
+    aps.make_nodes()
+    
+    no = vessel.control.nodes
+    print(no)
+    node_list.append(no)
+    execute_nodes()
+
+print(node_list)
+
+
+
+# execute_nodes()
+
+# for vessel in constellation_list:
 
 
 
 # # Set up streams for telemetry
 # ut = conn.add_stream(getattr, conn.space_center, 'ut')
-time.sleep(10)
-sc.quickload()
-conn.close()
+try:
+    time.sleep(30)
+except KeyboardInterrupt:
+    sc.quickload()
+finally:
+    conn.close()
 
 
-# direction = conn.add_stream(getattr, vessel.flight(), 'direction')
-# prograde = conn.add_stream(getattr, vessel.flight(), 'prograde')
 
 # vessel.auto_pilot.engage()
 # vessel.auto_pilot.sas = True

6135536006a797785f5b9cc5a93ff8d90b981559
os <os@noreply.invalid>
2022-08-15 10:23:08 +0200

    targeting nearest/2nd nearest relay antenna works with n(if n>=2) antennas

--- target_antennas.py
+++ target_antennas.py
@@ -29,137 +29,48 @@ for vessel in conn.space_center.vessels:
 # orbital period mean of constellation_list
 period_mean = sum(
     vessel.orbit.period for vessel in constellation_list) / len(constellation_list)
-print('Average period is {}'.format(period_mean))
 
 
 def get_telemetry():
 
     table = tabulate.tabulate([[i, v.name, v.orbit.body.name, v.orbit.apoapsis_altitude, v.orbit.periapsis_altitude, v.orbit.inclination, v.orbit.period, (v.orbit.period - period_mean)]
                               for i, v in enumerate(constellation_list)], headers=['Name', 'Body', 'MET', 'Apo', 'Per', 'Inclination', 'Period', 'Deviation'], tablefmt='fancy_grid')
-    print(table)
 
 
 get_telemetry()
 
-# for i, vessel in enumerate(constellation_list):
-    # # ccvessel.name = constellation_name + '_' + str(i)
-    # sc.active_vessel = switch_vessel(sc.active_vessel, vessel)
-
-
+distance_dict = {}
 #distance between each vesel in constellation_list
 for vessel in constellation_list:
+    distance_dict[vessel] = {}
     for target_vessel in constellation_list:
         if vessel is not target_vessel:
             sc.target_vessel = target_vessel
-            dist = sc.target_vessel.orbit.distance_at_closest_approach(vessel.orbit)
-            print('{} - {}: {}'.format(vessel.name, target_vessel.name, dist))
+            distance = sc.target_vessel.orbit.distance_at_closest_approach(vessel.orbit)
+            distance_dict[vessel].update({target_vessel: distance})
 
 
-# or vessel in constellation_list:
+for vessel, distance_to_vessel_dict in distance_dict.items():
+    sc.active_vessel = vessel
+    # switch_vessel(sc.active_vessel, vessel)
+    print('Switch to vessel' , vessel)
 
-    # for target_vessel in constellation_list[:-1]:
+    sorted_distance_to_vessel_dict = sorted(distance_to_vessel_dict.items(), key=lambda x: x[1])
 
+    antenna_parts = vessel.parts.with_tag('target_whatever')
 
+    for i, antenna_part in enumerate(antenna_parts):
+        antenna = conn.remote_tech.antenna(antenna_part)
 
-        # if vessel.name != target_vessel.name:
+        for module in antenna_part.modules:
+            if module.name == 'ModuleRTAntenna':
+                module.set_action('Activate')
 
-            # for part in vessel.parts.with_tag('relay'):
-                # antenna = conn.remote_tech.antenna(part)
-                # antenna.target_vessel = target_vessel
-
-
-    
-
-get_telemetry()
-
-
-
-
-
-
-
-
+        # even target nearest satellite
+        if i % 2 == 0:
+            antenna.target_vessel = sorted_distance_to_vessel_dict[0][0]
+        # uneven target 2nd nearest satellite
+        else:
+            antenna.target_vessel = sorted_distance_to_vessel_dict[1][0]
 
 
-# satellite operations
-#result = [orientate_vessel(conn, vessel, 'retrograde', block=False) for vessel in constellation_list]
-
-# solar
-# solar = [v.parts.solar_panels for v in constellation_list]
-# solar = [item for sublist in solar for item in sublist]
-# for panel in solar:
-# panel.deployed = True
-
-
-# mechanical jebediah time <3
-
-# def execute_nodes():
-# print("Executing nodes")
-# executor = mj.node_executor
-# # executor.execute_all_nodes()
-# executor.execute_one_node()
-
-# with conn.stream(getattr, executor, 'enabled') as enabled:
-# enabled.rate = 1
-# with enabled.condition:
-# while enabled():
-# enabled.wait()
-
-# node_list = []
-# for vessel in constellation_list:
-# sc.active_vessel = switch_vessel(sc.active_vessel, vessel)
-
-# planner = mj.maneuver_planner
-# man = planner.operation_circularize
-# # man.time_selector.time_reference = mj.TimeReference.
-# man.time_selector.circularize_altitude = 100000
-
-# ut = conn.add_stream(getattr, conn.space_center, 'ut')
-# # aps.ut = ut() + 100000
-
-# # aps.new_apoapsis = 200000
-
-# man.make_nodes()
-
-# no = vessel.control.nodes
-
-# for x in no:
-# print(x.ut)
-# node_list.append(no)
-# execute_nodes()
-
-# #print node list attributes delta_v, ut, remaining_burn_vector sort by ut
-# # node_list = sorted(node_list, key=lambda x: x.ut)
-# # for node in node_list:
-# # print(node.delta_v, node.ut, node.remaining_burn_vector)
-
-
-# # execute_nodes()
-
-# # for vessel in constellation_list:
-
-
-# # # Set up streams for telemetry
-# try:
-# time.sleep(30)
-# except KeyboardInterrupt:
-# sc.quickload()
-# finally:
-# conn.close()
-
-
-# # vessel.auto_pilot.engage()
-# # vessel.auto_pilot.sas = True
-# # auto_pilot.sas_mode = sc.SASMode.retrograde
-# # vessel.auto_pilot.wait()
-
-# # ut = 500
-# # control.add_node(ut, prograde=1.0)
-# # print(control.throttle)
-
-# #change apoapsis to desired value
-# # vessel.
-# # planner = mj.maneuver_planner
-# # apo = planner.operation_apoapsis
-# # apo.new_apoapsis = 1000000000
-# # apo.make_nodes()
